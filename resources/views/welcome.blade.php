d0aa14ec0e5ff63ede25a319de6555ffc1f21d4f
andrewhudson1977 <38694659+andrewhudson1977@users.noreply.github.com>
2025-07-15 22:05:43 +0100

    Update Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>We're Coming Soon</title>
+    <title>Your Developer</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
